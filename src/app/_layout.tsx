846652978e648c86ef42606dccc26192c1f969cf
wellarzin <brunodiaswwll@gmail.com>
2024-09-08 18:05:35 -0300

    clear imports

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,6 @@ import { Slot } from "expo-router";
 import { StatusBar } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as SplashScreen from "expo-splash-screen"
-import { useEffect, useCallback } from "react";
 import { useFonts } from "expo-font";
 
 SplashScreen.preventAutoHideAsync();
