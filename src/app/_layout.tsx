f7f529ab149fc80c9e54c1b35c0280e4f091424f
wellarzin <brunodiaswwll@gmail.com>
2024-09-08 17:54:05 -0300

    test

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,35 +5,23 @@ import * as SplashScreen from "expo-splash-screen"
 import { useEffect, useCallback } from "react";
 import { useFonts } from "expo-font";
 
-
 SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
     Raleway: require("../../assets/fonts/Raleway-Bold.ttf"),
-  });
+  })
 
-// Função para ocultar o splash screen após carregar as fontes
-// basicamente esse bloco serve somente para carregar o conteúdo da aplicação somente depois que as fontes forem carregadas
-// para não quebrar nada do estilo da aplicação
-  const onFontsLoaded = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  useEffect(() => {
-    onFontsLoaded();
-  }, [onFontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null; 
+  if(!fontsLoaded){
+    return null;
   }
+
+  SplashScreen.hideAsync();
   return (
     <>
       <GestureHandlerRootView>
         <StatusBar barStyle="light-content"/>
-        {fontsLoaded && <Slot />}
+        <Slot />
       </GestureHandlerRootView>
     </>
   );
